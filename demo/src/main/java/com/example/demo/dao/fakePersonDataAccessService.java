dd6ef34b93c4039fd31dce24787f5b3d09c1c70a
Xenon <s10110040@gmail.com>
2022-04-11 01:44:24 +0800

    first spring post request

--- demo/src/main/java/com/example/demo/dao/fakePersonDataAccessService.java
+++ demo/src/main/java/com/example/demo/dao/fakePersonDataAccessService.java
@@ -1,13 +1,16 @@
-package com.example.demo.model;
-
-import com.example.demo.dao.PersonDao;
+package com.example.demo.dao;
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
+@SuppressWarnings("unused")
 public class fakePersonDataAccessService implements PersonDao {
-    static private List<Person> DB = new ArrayList<>();
+    static private final List<Person> DB = new ArrayList<>();
+
     @Override
     public int insertPerson(UUID uuid, Person person) {
         DB.add(new Person(uuid, person.getName()));
--- demo/src/main/java/com/example/demo/service/PersonService.java
+++ demo/src/main/java/com/example/demo/service/PersonService.java
@@ -2,14 +2,20 @@ package com.example.demo.service;
 
 import com.example.demo.dao.PersonDao;
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonService {
-    private PersonDao personDao;
+    private final PersonDao personDao;
 
-    public PersonService(PersonDao personDao) {
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+
         this.personDao = personDao;
     }
-
+    @SuppressWarnings("UnusedReturnValue")
     public int addPerson(Person person){
         return this.personDao.insertPerson(person);
     }
